f4920a67cfa349ec940915da0f064712a1c9453b
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-17 11:09:18 -0300

    :sparkles: import database handler

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,12 +1,4 @@
-let profileData = {
-  name: 'Wendell',
-  avatar: 'https://avatars.githubusercontent.com/u/72822574?v=4',
-  'monthly-budget': 3000,
-  'days-per-week': 5,
-  'vacation-per-year': 4,
-  'hours-per-day': 5,
-  'hour-value': 50
-};
+const Database = require('../db/config');
 
 module.exports = {
   getData() {
