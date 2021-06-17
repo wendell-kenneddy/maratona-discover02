d28cc5365caa70b39a36711543e6af583bf20e87
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-17 11:09:58 -0300

    :hammer: update getData at profile to use the sqlite db

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,8 +1,20 @@
 const Database = require('../db/config');
 
 module.exports = {
-  getData() {
-    return profileData;
+  async getData() {
+    const db = await Database();
+    const profileData = await db.get(`SELECT * FROM profile`);
+    const normalizedData = {
+      name: profileData.name,
+      avatar: profileData.avatar,
+      'monthly-budget': profileData.monthly_budget,
+      'days-per-week': profileData.days_per_week,
+      'hours-per-day': profileData.hours_per_day,
+      'vacation-per-year': profileData.vacation_per_year,
+      'hour-value': profileData.hour_value
+    }
+    await db.close();
+    return normalizedData;
   },
 
   update(newData) {
